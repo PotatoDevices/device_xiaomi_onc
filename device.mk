bb4ac209c99bbf4395f6990a4c1f842ba20c2147
Gagan Malvi <malvigagan@gmail.com>
2022-01-13 12:11:06 +0100

    onc: Refactor USB HAL for codename.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -388,7 +388,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.msm8953
+    android.hardware.usb@1.0-service.onc
 
 # Vibrator
 PRODUCT_PACKAGES += \
